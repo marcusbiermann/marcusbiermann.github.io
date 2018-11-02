fc2d7654d5674f0f3837ccdf7200d76e3003fbe6
Philipp Siekmann <siekmann@neoskop.de>
2018-11-02 13:23:45 +0100

    initial commit

---
+++ site/content/_index.md
@@ -0,0 +1,78 @@
+---
+home:
+  contact:
+    email: 'm.biermann@lse.ac.uk '
+  cv: 'https://drive.google.com/open?id=14Scl3GwyI0ojuLoPhkom_WS4dWys9jGE'
+  intro:
+    text: >-
+      I am a job market candidate in economics at the London School of Economics
+      and affiliated with the Centre for Economic Performance. My primary
+      research field is international trade. My secondary research interests are
+      economic history and finance.
+  job_paper: 'https://drive.google.com/open?id=1n1-pd4CxyauSDExvsukhXQWQe9wXTWAK'
+  large_photo: /img/portrait_original.png
+  locations:
+    - text: the European Job Market in Naples.
+    - text: and the ASSA Annual Meeting in Atlanta.
+  small_photo: /img/portrait.jpg
+  title: Marcus Biermann
+papers:
+  - description: >-
+      We study whether multinational corporations transmit the effects of a
+      localized banking crisis to countries all over the world. We identify an
+      exogenous shock to the credit supply of multinational parent corporations
+      located in Germany. The shock to parents caused a reduction in the sales
+      of their international affiliates for three years. We use unique data on
+      linkages between parents and affiliates to study the transmission
+      mechanism from parents’ credit supply to affiliates’ sales. Both financial
+      and real channels played a role: Parents withdrew equity from their
+      affiliates, reduced intra-firm trade with affiliates, and required more
+      long-term lending from the affiliates. The results suggest that
+      multinationals contribute to global business cycle synchronization.
+    headline: Job Market Paper
+    link: 'https://drive.google.com/open?id=1n1-pd4CxyauSDExvsukhXQWQe9wXTWAK'
+    subtitle: (with Kilian Huber)
+    title: Multinational Corporations and the International Transmission of Crises
+  - description: >-
+      What is the effect of trade on the size distribution of firms? I collected
+      historical data between 1882 and 1907 from the German Empire to address
+      this question. I can then match three data sets according to the same
+      geographic boundaries: industry census data, railway and waterway trade
+      data. The key findings are that trade integration impacts the firm size
+      distribution heterogeneously across five size categories. I find evidence
+      of a hierarchical and stark shift in employment and firm share from the
+      smallest towards larger firm size categories. A “Bartik” instrument is
+      proposed to argue that the correlations described are indeed causal. I
+      provide evidence for a fall in transport costs and technology adoption as
+      mechanisms to explain the stylized facts observed in the data.
+    headline: Working Paper
+    link: 'https://drive.google.com/open?id=1ExXuBNsiGKpcsrGVD7hx-fwizl5VKYHw'
+    subtitle: (Revise and resubmit at the Canadian Journal of Economics)
+    title: 'Trade and the Size Distribution of Firms: Evidence from the German Empire '
+cv:
+  pre_doctoral_studies:
+    - item: 'Master of Research in Economics, LSE (2013)'
+    - item: 'Master of Science in Economics (<em>with distinction</em>), LSE (2012)'
+    - item: 'Bachelor of Science in Economics, University of Bonn (2011)'
+  primary_research_fields: International Trade
+  secondary_research_fields: 'Economic History, Finance'
+teaching:
+  tables:
+    - headline: London School of Economics
+      rows:
+        - date: 2017-2018
+          title: 'International Economics (EC421), MSc course'
+        - date: '2016'
+          title: 'International Economics (EC351), Summer school'
+        - date: '2013-2014, 2015-2017'
+          title: 'International Economics (EC315), 3rd-year undergraduate level'
+    - headline: Teaching Certification
+      rows:
+        - date: 2016-2017
+          title: Postgraduate Certificate in Higher Education (Associate Level)
+    - headline: University of Bonn
+      rows:
+        - date: 2010-2011
+          title: Statistics and Econometrics (undergraduate)
+---
+
---
+++ src/js/cms-preview-templates/home.js
@@ -0,0 +1,184 @@
+import React from 'react';
+
+export default class PostPreview extends React.Component {
+  render() {
+    const { entry, getAsset } = this.props;
+
+    return (
+      <div>
+        <section id="home" class="section">
+          <div>
+            <div class="columns">
+              <header class="header">
+                <a href={getAsset(entry.getIn(['data', 'home', 'large_photo']))} target="_blank">
+                  <img
+                    width="200"
+                    src={getAsset(entry.getIn(['data', 'home', 'small_photo']))}
+                    alt="Portrait: Marcus Biermann"
+                  />
+                </a>
+              </header>
+
+              <div>
+                <h1>{entry.getIn(['data', 'home', 'title'])}</h1>
+                <p>
+                  <strong>{entry.getIn(['data', 'home', 'intro', 'text'])}</strong>
+                </p>
+
+                <h3>I am available for interviews at</h3>
+
+                <ul>
+                  {entry.getIn(['data', 'home', 'locations']).map(location => (
+                    <li>{location.getIn(['text'])}</li>
+                  ))}
+                </ul>
+
+                <p class="download">
+                  <a href={entry.getIn(['data', 'home', 'job_paper'])} target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="icon"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                      <polyline points="7 10 12 15 17 10" />
+                      <line x1="12" y1="15" x2="12" y2="3" />
+                    </svg>
+                    Download Job Market Paper
+                  </a>
+                </p>
+
+                <h3>Contact</h3>
+                <address>
+                  email:{' '}
+                  <a href={'mailto:' + entry.getIn(['data', 'home', 'contact', 'email'])}>
+                    {entry.getIn(['data', 'home', 'contact', 'email'])}
+                  </a>
+                  <br />
+                  Department of Economics
+                  <br />
+                  London School of Economics and Political Science
+                  <br />
+                  Houghton Street
+                  <br />
+                  London WC2A 2AE
+                </address>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="research" class="section">
+          <div>
+            <header class="header">
+              <h2>
+                <span>Research</span>
+              </h2>
+            </header>
+
+            {entry.getIn(['data', 'papers']).map(paper => (
+              <article class="paper">
+                <header>
+                  <h3>{paper.getIn(['headline'])}</h3>
+                  <a href={paper.getIn(['link'])} target="_blank">
+                    <span>{paper.getIn(['title'])}</span>
+                  </a>
+                  <br />
+                  {paper.getIn(['subtitle'])}
+                </header>
+                ^<p>{paper.getIn(['description'])}</p>
+              </article>
+            ))}
+          </div>
+        </section>
+
+        <section id="cv" class="section">
+          <div>
+            <header class="header">
+              <h2>
+                <span>CV</span>
+              </h2>
+            </header>
+
+            <p class="download">
+              <a href={entry.getIn(['data', 'home', 'cv'])} target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                  <polyline points="7 10 12 15 17 10" />
+                  <line x1="12" y1="15" x2="12" y2="3" />
+                </svg>
+                Full CV (pdf)
+              </a>
+            </p>
+
+            <h3>Short CV</h3>
+
+            <p>
+              <strong>Primary Research Field:</strong>
+              <br /> {entry.getIn(['data', 'cv', 'primary_research_fields'])}
+              <br />
+              <br />
+              <strong>Secondary Research Fields:</strong>
+              <br /> {entry.getIn(['data', 'cv', 'secondary_research_fields'])}
+              <br />
+              <br />
+              <strong>Pre-Doctoral Studies:</strong>
+              <br />{' '}
+              {entry.getIn(['data', 'cv', 'pre_doctoral_studies']).map(study => (
+                <React.Fragment>
+                  <span dangerouslySetInnerHTML={{ __html: study.getIn(['item']) }} />
+                  <br />
+                </React.Fragment>
+              ))}
+            </p>
+          </div>
+        </section>
+
+        <section id="teaching" class="section">
+          <div>
+            <header class="header">
+              <h2>
+                <span>Teaching</span>
+              </h2>
+            </header>
+
+            {entry.getIn(['data', 'teaching', 'tables']).map(table => (
+              <React.Fragment>
+                <h3>{table.getIn(['headline'])}</h3>
+
+                <table class="timetable">
+                  <tbody>
+                    {table.getIn(['rows']).map(row => (
+                      <tr>
+                        <td>{row.getIn(['date'])}</td>
+                        <td>{row.getIn(['title'])}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </React.Fragment>
+            ))}
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
